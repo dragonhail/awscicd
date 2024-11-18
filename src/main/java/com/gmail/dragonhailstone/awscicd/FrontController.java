c66fbf1fe6a4a52f0b1b7349f9d29fd32079001e
dragonhail <dragonhailstone@gmail.com>
2024-11-18 15:32:33 +0900

    test

--- src/main/java/com/gmail/dragonhailstone/awscicd/FrontController.java
+++ src/main/java/com/gmail/dragonhailstone/awscicd/FrontController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class FrontController {
     @RequestMapping("/")
     public String index() {
-        return "Hello World";
+        return "Hello World!";
     }
 }
